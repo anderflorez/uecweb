ff93f69c34a03e4f5748093e57c4d7a961999447
Anderson <anderson@unlimitedcompanies.com>
2016-11-29 09:11:43 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+	$(".carousel").carousel({
+		pause: false
+	});
+
+	$("#uec").css("line-height", $("#sec1").css("height"));
+	$("#uee").css("line-height", $("#sec2").css("height"));
+	$("#isuites").css("line-height", $("#sec3").css("height"));
+});
