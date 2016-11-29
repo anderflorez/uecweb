00012c854e0f58757ffac2eda693a3d2f91cee0e
Anderson <anderson@unlimitedcompanies.com>
2016-11-29 17:18:41 -0500

    working on making home page content mobile friendly, still missing mobile proper placing of elements

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(function() {
 		pause: false
 	});
 
-	$("#uec").css("line-height", $("#sec1").css("height"));
-	$("#uee").css("line-height", $("#sec2").css("height"));
-	$("#isuites").css("line-height", $("#sec3").css("height"));
+	if ($("html").width() > 750) {
+		$("#uec").css("line-height", $("#sec1").css("height"));
+		$("#uee").css("line-height", $("#sec2").css("height"));
+		$("#isuites").css("line-height", $("#sec3").css("height"));
+	}
 });
