e92bbae20f2a1f062638ba6081956c382ece8b55
Anderson <anderson@unlimitedcompanies.com>
2016-12-02 17:37:20 -0500

    finished for the week

--- js/script.js
+++ js/script.js
@@ -13,16 +13,31 @@ $(function() {
 		$("#brand3").insertBefore("#isuites");
 	}
 
-	$("#map").css("background-color", "yellow");
-	function initMap() {
-		var uluru = {lat: -25.363, lng: 131.044};
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: uluru
+	var project=new google.maps.LatLng(25.935078,-80.121629);
+	var center=new google.maps.LatLng(26.256238,-80.323836);
+
+	function initialize(){
+		var mapProp = {
+			center:project,
+			zoom:12,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+
+		var map=new google.maps.Map(document.getElementById("map"),mapProp);
+
+		var marker=new google.maps.Marker({
+			position:project,
+			icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
 		});
-		var marker = new google.maps.Marker({
-			position: uluru,
-			map:map
+		
+		var marker1=new google.maps.Marker({
+			position:hCenter1,
+			icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
 		});
+
+		marker.setMap(map);
+		marker1.setMap(map);
 	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
 });
