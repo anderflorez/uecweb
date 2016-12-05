b78a7106ad39a72924e2b323557045c8acfa43eb
Anderson <anderson@unlimitedcompanies.com>
2016-12-05 17:06:51 -0500

    All pages have been completed except for the projects

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(function() {
 		$("#uec").css("line-height", $("#sec1").css("height"));
 		$("#uee").css("line-height", $("#sec2").css("height"));
 		$("#isuites").css("line-height", $("#sec3").css("height"));
+		$("#uec_name").css("line-height", $("#uec_logo"));
 	}
 	else {
 		$("#brand1").insertBefore("#uec");
