edaf9de7121622bbe791887d9fef8811defcfb05
Anderson <anderson@unlimitedcompanies.com>
2016-12-13 15:41:45 -0500

    Finished with the three different models to choose the design, waiting for Management decision

--- v1.2/js/script.js
+++ v1.2/js/script.js
@@ -7,10 +7,11 @@ $(function() {
 		$("#uec").css("line-height", $("#sec1").css("height"));
 		$("#uee").css("line-height", $("#sec2").css("height"));
 		$("#isuites").css("line-height", $("#sec3").css("height"));
-		$("#uec_name").css("line-height", $("#uec_logo"));
 	}
 	else {
 		$("#brand1").insertBefore("#uec");
 		$("#brand3").insertBefore("#isuites");
 	}
+
+	$("#uc_logos").css("line-height", $("nav").css("height"));
 });
